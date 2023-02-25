3b7d2a9f5c61f1be68e136d55bd6a72b251e0daf
logonoff <logonoff@users.noreply.github.com>
2023-02-25 02:53:43 -0500

    add `ExpiryContainer` and fix workshop responsive
    ExpiryContainer exists so team pages disappear after the year ends

--- frontend/src/components/index.jsx
+++ frontend/src/components/index.jsx
@@ -1,13 +1,14 @@
 /**
  * @fileoverview This file exports all the components in the components folder
  */
-export { HeroHeader } from './HeroHeader/HeroHeader';
 export { ConvertDate } from './ConvertDate/ConvertDate';
 export { default as Team } from './Team/Team';
 export { ErrorBoundary } from './ErrorBoundary/ErrorBoundary';
 export { EventList } from './EventList/EventList';
+export { ExpiryContainer } from './ExpiryContainer/ExpiryContainer';
 export { FAQ } from './FAQ/FAQ';
 export { Footer } from './Footer/Footer';
+export { HeroHeader } from './HeroHeader/HeroHeader';
 export { ImageLinkCard } from './ImageLinkCard/ImageLinkCard';
 export { InfoCard } from './InfoCard/InfoCard';
 export { JoinAnd } from './JoinAnd/JoinAnd';
