505e11c113c294e9ebaddb74f4c3761bb2c62973
logonoff <logonoff@users.noreply.github.com>
2023-03-11 22:38:08 -0500

    Update github_action_add_workshop.js

--- github_action_add_workshop.js
+++ github_action_add_workshop.js
@@ -8,15 +8,17 @@ async function run() {
             name: process.env['workshop_name'],
             date: process.env['workshop_date'],
             host: process.env['workshop_host'].split(","),
-            description: process.env['workshop_description'],
-            slides: process.env['workshop_slides'],
-            recording: process.env['workshop_recording'],
-            code: process.env['workshop_code'],
+            description: process.env['workshop_description']
         };
+        if (process.env['workshop_slides']) { workshop.slides: process.env['workshop_slides']; }
+        if (process.env['workshop_recording']) { workshop.recording: process.env['workshop_recording']; }
+        if (process.env['workshop_code']) { workshop.code: process.env['workshop_code']; }
+        workshop.host = workshop.host.map(s => s.trim());
+        workshop.host = workshop.host.filter(s => s.trim()); // removes empty strings
+
         const temp = JSON.parse(fs.readFileSync('./frontend/src/data/workshops.json'))
         temp[process.env['category']].push(workshop)
         fs.writeFileSync('./frontend/src/data/workshops.json', JSON.stringify(temp, null, 2))
-
     } catch (error) {
         core.setFailed(error.message);
     }
