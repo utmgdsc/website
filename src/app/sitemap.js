f71505804559aa7ee6f02825660786f3b7038b55
logonoff <logonoff@users.noreply.github.com>
2023-06-30 23:39:55 -0400

    Create sitemap.js

---
+++ src/app/sitemap.js
@@ -0,0 +1,22 @@
+import { pages } from "../data/NavbarTabData"
+
+const BASEURL = "https://gdscutm.com"
+
+/**
+ * Generates the sitemap for the website. Note that a
+ * HACK is that the baseurl is hardcoded to prod.
+ *
+ * Uses the NavbarTabData to generate the sitemap.
+ *
+ * @returns {Array<{url: string, lastModified: Date}} The sitemap
+ */
+export default function sitemap() {
+    let acc = [];
+    pages.forEach((item) => {
+        acc.push({
+            url: `${BASEURL}${item.path}`,
+            lastModified: new Date(),
+        })
+    })
+    return acc;
+}
