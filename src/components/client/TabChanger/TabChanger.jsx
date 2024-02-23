78f7a4377dd2461ec549bc412320dd0e4f688eec
logonoff <18614559+logonoff@users.noreply.github.com>
2024-02-22 20:49:34 -0500

    prune unused component

--- src/components/client/TabChanger/TabChanger.jsx
+++ src/components/client/TabChanger/TabChanger.jsx
@@ -1,5 +1,4 @@
 import { Tabs, Tab } from '@mui/material';
-import { Link } from '@/components/server';
 
 /**
  * TabChanger component for MUI tabs
@@ -28,33 +27,6 @@ export const TabChanger = ({ tabList, page, setPage }) => {
 		>
 			{tabList.map((year, id) => (
 				<Tab key={id} label={year} />
-			))}
-		</Tabs>
-	);
-};
-
-/**
- * TabChanger component for MUI tabs
- * @property {string[]} tabList list of years
- * @property {integer} page current page
- * @property {string} prefix prefix to add to the href
- * @returns {JSX.Element} TabChanger component
- */
-export const LinkBasedTabChanger = ({ tabList, page, prefix }) => {
-	// if there is only one tab, don't show tabs
-	if (tabList.length <= 1) {
-		return <></>;
-	}
-
-	return (
-		<Tabs
-			value={page}
-			sx={{
-				my: '1rem',
-			}}
-		>
-			{tabList.map((year, id) => (
-				<Tab key={id} label={year} component={Link} href={`${prefix}/${year}`} scroll={false} />
 			))}
 		</Tabs>
 	);
