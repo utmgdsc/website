8e04a27c0b4a37248cf115eb6b32f821736d527d
logonoff <git@logonoff.co>
2024-11-08 10:03:40 -0500

    feat: ical backend route

---
+++ src/app/api/events/route.js
@@ -0,0 +1,139 @@
+import ical from 'ical-generator';
+
+/**
+ * Minimum date allowed by JavaScript Date object.
+ * @type {Date}
+ * @see https://262.ecma-international.org/5.1/#sec-15.9.1.1
+ */
+export const MIN_DATE = new Date(-8640000000000000);
+
+/**
+ * Maximum date allowed by JavaScript Date object.
+ * @type {Date}
+ * @see https://262.ecma-international.org/5.1/#sec-15.9.1.1
+ */
+export const MAX_DATE = new Date(8640000000000000);
+
+/**
+ * load external json file from api
+ * @param {integer} limit the number of events to show
+ * @param {Date} from the date to start showing events from (inclusive), based on end_date
+ * @param {Date} to the date to stop showing events at (non-inclusive), based on end_date
+ * @returns {object} bevy chapter object
+ */
+const getEvents = async (limit, from, to) => {
+	return await fetch(process.env.CHAPTER_API_URL, {
+		next: { revalidate: 3600 }, // revalidate once an hour
+	})
+		.then(response => response.json())
+		.then(data => {
+			if (data['detail'] && (data['detail'].includes('throttled') || data['detail'].includes('error'))) {
+				throw new Error(data['detail']);
+			}
+			// filter only published events
+			let eventData = data['results'].filter(event => event['status'] === 'Published');
+
+			// slice to limit if specified
+			if (limit) {
+				eventData = eventData.slice(0, limit);
+			}
+
+			// filter only upcoming events if specified
+			eventData = eventData.filter(event => {
+				const endDate = new Date(event['end_date']);
+				return endDate >= from && endDate < to;
+			});
+
+			return eventData;
+		})
+		.catch(error => {
+			return error;
+		});
+};
+
+/**
+ * load external json file from api
+ * @param {id} id id of the event
+ * @returns {object} bevy event object
+ */
+const getDescription = async id => {
+	return await fetch(process.env.EVENT_API_URL + id, {
+		next: { revalidate: 604800 }, // revalidate once a week
+	})
+		.then(response => response.json())
+		.then(data => {
+			if (!data['description_short']) {
+				return data['message'];
+			}
+
+			return data['description_short'];
+		})
+		.catch(error => {
+			return error?.message;
+		});
+};
+
+/**
+ * get descriptions for events from api
+ */
+export const getDescriptions = async (limit, from, to) => {
+	const events = await getEvents(limit, from, to);
+
+	const descriptions = await Promise.all(
+		events.map(async event => {
+			return await getDescription(event['id']);
+		})
+	);
+
+	return { events, descriptions };
+};
+
+/**
+ * Gets a list of years that have events.
+ *
+ * @returns {Array} array of years
+ */
+export const getYears = async () => {
+	const events = await getEvents(undefined, MIN_DATE, new Date());
+
+	const years = events.reduce((acc, event) => {
+		const year = new Date(event['end_date']).getFullYear();
+		if (!acc.includes(year)) {
+			acc.push(year);
+		}
+		return acc;
+	}, []);
+
+	return years.sort((a, b) => b - a);
+};
+
+export async function GET(req, res) {
+	if (req.method !== 'GET') {
+		res.status(405).end();
+		return;
+	}
+
+	const calendar = ical({
+		name: 'GDSC UTM Events',
+	});
+
+	const { events, descriptions } = await getDescriptions(undefined, MIN_DATE, MAX_DATE);
+
+	events.forEach((event, id) => {
+		calendar.createEvent({
+			start: new Date(event['start_date']),
+			end: new Date(event['end_date']),
+			summary: event['title'],
+			description: descriptions[id],
+			url: event['url'],
+		});
+	});
+
+	return new Response(calendar.toString(), {
+		status: 200,
+		headers: {
+			'Content-Type': 'text/calendar',
+			'Content-Disposition': 'attachment; filename="gdsc-utm-events.ics"',
+		},
+	});
+}
--- src/components/server/EventList/EventList.jsx
+++ src/components/server/EventList/EventList.jsx
@@ -1,79 +1,7 @@
 import { ConvertDate } from '~/components/server';
 import { InfoCard } from '~/components/client';
 import { Alert, Grid, Typography } from '@mui/material';
-
-/**
- * Minimum date allowed by JavaScript Date object.
- * @type {Date}
- * @see https://262.ecma-international.org/5.1/#sec-15.9.1.1
- */
-const MIN_DATE = new Date(-8640000000000000);
-
-/**
- * Maximum date allowed by JavaScript Date object.
- * @type {Date}
- * @see https://262.ecma-international.org/5.1/#sec-15.9.1.1
- */
-const MAX_DATE = new Date(8640000000000000);
-
-/**
- * load external json file from api
- * @param {integer} limit the number of events to show
- * @param {Date} from the date to start showing events from (inclusive), based on end_date
- * @param {Date} to the date to stop showing events at (non-inclusive), based on end_date
- * @returns {object} bevy chapter object
- */
-const getEvents = async (limit, from, to) => {
-	return await fetch(process.env.CHAPTER_API_URL, {
-		next: { revalidate: 3600 }, // revalidate once an hour
-	})
-		.then(response => response.json())
-		.then(data => {
-			if (data['detail'] && (data['detail'].includes('throttled') || data['detail'].includes('error'))) {
-				throw new Error(data['detail']);
-			}
-			// filter only published events
-			let eventData = data['results'].filter(event => event['status'] === 'Published');
-
-			// slice to limit if specified
-			if (limit) {
-				eventData = eventData.slice(0, limit);
-			}
-
-			// filter only upcoming events if specified
-			eventData = eventData.filter(event => {
-				const endDate = new Date(event['end_date']);
-				return endDate >= from && endDate < to;
-			});
-
-			return eventData;
-		})
-		.catch(error => {
-			return error;
-		});
-};
-
-/**
- * load external json file from api
- * @param {id} id id of the event
- * @returns {object} bevy event object
- */
-const getDescription = async id => {
-	return await fetch(process.env.EVENT_API_URL + id, {
-		next: { revalidate: 604800 }, // revalidate once a week
-	})
-		.then(response => response.json())
-		.then(data => {
-			if (!data['description_short']) {
-				return data['message'];
-			}
-
-			return data['description_short'];
-		})
-		.catch(error => {
-			return error?.message;
-		});
-};
+import { MIN_DATE, MAX_DATE, getDescriptions, getYears } from '~/app/api/events/route';
 
 const EventInfoCard = ({ event, description }) => {
 	return (
@@ -84,25 +12,6 @@ const EventInfoCard = ({ event, description }) => {
 			description={description}
 		/>
 	);
-};
-
-/**
- * Gets a list of years that have events.
- *
- * @returns {Array} array of years
- */
-export const getYears = async () => {
-	const events = await getEvents(undefined, MIN_DATE, new Date());
-
-	const years = events.reduce((acc, event) => {
-		const year = new Date(event['end_date']).getFullYear();
-		if (!acc.includes(year)) {
-			acc.push(year);
-		}
-		return acc;
-	}, []);
-
-	return years.sort((a, b) => b - a);
 };
 
 /**
@@ -117,13 +26,7 @@ export const getYears = async () => {
  * @returns {JSX.Element} EventList component
  */
 export const EventList = async ({ limit, from = MIN_DATE, to = MAX_DATE, EmptyComponent = null }) => {
-	const events = await getEvents(limit, from, to);
-
-	const descriptions = await Promise.all(
-		events.map(async event => {
-			return await getDescription(event['id']);
-		})
-	);
+	const { events, descriptions } = await getDescriptions(limit, from, to);
 
 	if (!Array.isArray(events)) {
 		return <Alert severity="error">{events?.message}</Alert>;
