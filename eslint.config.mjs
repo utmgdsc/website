c5a0606cf9bdd898702405d3f858848a50aff086
logonoff <git@logonoff.co>
2024-12-09 10:30:01 -0500

    chore(deps): upgrade to react 19

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,23 +1,29 @@
-import { FlatCompat } from '@eslint/eslintrc';
+import globals from 'globals';
 import eslint from '@eslint/js';
 import pluginPromise from 'eslint-plugin-promise';
 import reactPlugin from 'eslint-plugin-react';
-
-const compat = new FlatCompat({
-	baseDirectory: import.meta.dirname,
-});
+import pluginNext from '@next/eslint-plugin-next';
 
 const eslintConfig = [
 	eslint.configs.recommended,
 	pluginPromise.configs['flat/recommended'],
 	reactPlugin.configs.flat.recommended,
 	reactPlugin.configs.flat['jsx-runtime'],
-	...compat.extends('next/core-web-vitals', 'next/typescript'),
 	{
+		plugins: {
+			'@next/next': pluginNext,
+		},
+		rules: {
+			...pluginNext.configs.recommended.rules,
+			...pluginNext.configs['core-web-vitals'].rules,
+		},
 		languageOptions: {
 			ecmaVersion: 'latest',
 			sourceType: 'module',
-
+			globals: {
+				...globals.browser,
+				...globals.node,
+			},
 			parserOptions: {
 				ecmaFeatures: {
 					jsx: true,
