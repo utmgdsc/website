84dd23b056d3d45f8b739cfb10e3e7e247faf791
GBG7 <dryeeti4@gmail.com>
2026-01-11 17:49:15 -0500

    FIX: update gdscwordmark.svg and a few other changes

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -51,7 +51,7 @@ const ProjectPage = () => {
 					You could potentially earn a credit for CSC392/CSC492 while working on your project under faculty
 					guidance! Additionally, you may be able to submit your project as a part of the global{' '}
 					<Link external href="https://developers.google.com/community/gdsc-solution-challenge">
-						GDG UTM Solution challenge
+						GDG Solution challenge
 					</Link>{' '}
 					and win prizes!
 				</Typography>
